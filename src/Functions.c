ef810e533f01380e37733bf918f3e990817e116b
stepin105083 <69079338+stepin105083@users.noreply.github.com>
2021-02-05 14:20:20 +0530

    Update Functions.c

--- src/Functions.c
+++ src/Functions.c
@@ -2,24 +2,24 @@
 #include "hello.h"
  void Anagha()
 {
-    printf("I am Anagha");
+    printf("I am Anagha\n");
 
 }
 
 void Pranitha()
 {
-    printf("This is Pranitha");
+    printf("This is Pranitha\n");
 }
 
 void Fayaz()
 {
-    printf("Iam Fayaz");
+    printf("Iam Fayaz\n");
 }
 
 void Karti(){
-    printf("Hi, I'm Karti!");
+    printf("Hi, I'm Karti!\n");
 }
 
 void Gagana(){
-    printf("Hi, I'm Gagana!");
+    printf("Hi, I'm Gagana!\n");
 }
